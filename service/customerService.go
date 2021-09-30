3c8b9178fd30aa3c8a273687ef466b95d62e01e9
d.lozhkin <d.lozhkin@geekbrains.ru>
2021-09-30 09:34:56 +0300

    dto

--- service/customerService.go
+++ service/customerService.go
@@ -7,7 +7,7 @@ import (
 )
 
 type CustomerService interface {
-	GetAllCustomers() ([]domain.Customer, *errs.AppError)
+	GetAllCustomers() ([]dto.CustomerResponse, *errs.AppError)
 	GetCustomer(string) (*dto.CustomerResponse, *errs.AppError)
 }
 
@@ -15,8 +15,20 @@ type DefaultCustomerService struct {
 	repo domain.CustomerRepository
 }
 
-func (s DefaultCustomerService) GetAllCustomers() ([]domain.Customer, *errs.AppError) {
-	return s.repo.FindAll()
+func (s DefaultCustomerService) GetAllCustomers() ([]dto.CustomerResponse, *errs.AppError) {
+	c, err := s.repo.FindAll()
+
+	if err != nil {
+		return nil, err
+	}
+
+	var response []dto.CustomerResponse
+
+	for _, v := range c {
+		response = append(response, v.ToDto())
+	}
+
+	return response, nil
 }
 
 func (s DefaultCustomerService) GetCustomer(id string) (*dto.CustomerResponse, *errs.AppError) {
